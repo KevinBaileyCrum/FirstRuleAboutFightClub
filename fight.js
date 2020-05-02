f2d8efd5566bd32a2c91eaa23dd6c28048aff347
Kevin Crum <kevinbaileycrum@gmail.com>
2020-05-02 14:41:57 -0700

    candidates pasted

---
+++ fight.js
@@ -0,0 +1,23 @@
+const candidates = [
+["Name","Health","Damage"],
+["Tom Cruise",136,6],
+["Sponge Bob",110,4],
+["James Earl Jones",175,8],
+["Bob Barker",112,2],
+["Tonya Harding",108,7],
+["Charles Barkley",220,12],
+["Peter Piper",116,4],
+["Harry Potter",96,16],
+["Shamu",280,24],
+["Bill Gates",124,6],
+];
+
+// main
+const main = () => {
+   return candidates
+}
+
+// if __name__ == '__main__'
+if (require.main === module) {
+   main();
+}
