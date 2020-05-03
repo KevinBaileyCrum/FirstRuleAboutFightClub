eadb9afbf612c71e08e3ad906c62632ead96d2b8
Kevin Crum <kevinbaileycrum@gmail.com>
2020-05-02 17:16:08 -0700

    all done

--- fight.js
+++ fight.js
@@ -1,4 +1,4 @@
-const defaultCandidates = [
+const candidates = [
    ["Tom Cruise",136,6],
    ["Sponge Bob",110,4],
    ["James Earl Jones",175,8],
@@ -11,30 +11,14 @@ const defaultCandidates = [
    ["Bill Gates",124,6],
 ]
 
-const parseArgs = (args) => {
-   if (args.length == 0) {
-      return defaultCandidates
-   } else if (args.length == 1) {
-      const fs = require('fs')
-      const filename = args[0]
-      const candidates = fs.readFileSync(args[0], {encoding:'utf8', flag:'r'})
-      return candidates
-
-
-   } else {
-      console.log('usage: node fight.js [filename]\nfile name is optional')
-      process.exit()
-   }
-}
-
 // Durstenfeld shuffle
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-const shuffleCandidates = (candidates) => {
-   for (let i = candidates.length - 1; i > 0; i--) {
+const shuffleFighters = (fighters) => {
+   for (let i = fighters.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [candidates[i], candidates[j]] = [candidates[j], candidates[i]];
+      [fighters[i], fighters[j]] = [fighters[j], fighters[i]];
    }
-   return candidates
+   return fighters
 }
 
 const slap = (hitter, receiver) => {
@@ -43,7 +27,7 @@ const slap = (hitter, receiver) => {
 
 const fight = (fighters) => {
    // randomize order for who strikes first
-   shuffleCandidates(fighters)
+   shuffleFighters(fighters)
    let turn = 0
    while ((fighters[0][1] > 0) && (fighters[1][1] > 0)) {
       slap(fighters[turn % 2], fighters[(turn + 1) % 2])
@@ -59,26 +43,16 @@ const fight = (fighters) => {
 // main
 const main = () => {
    let winnerMap = new Map()
-   let candidates = parseArgs(process.argv.slice(2))
    candidates.forEach(contestant => {
       const opponents = candidates.filter(opponent => candidates.indexOf(opponent) > candidates.indexOf(contestant))
       if (opponents.length > 0) {
          opponents.forEach(opponent => {
             winner = fight([contestant.slice(0), opponent.slice(0)])
-            if (winner[0] === contestant[0]) {
-               if (winnerMap.has(contestant[0])) {
-                  winnerMap.get(contestant[0]).wins++
-               } else {
-                  winnerMap.set(contestant[0], {wins: 1})
-               }
+            if (winnerMap.has(winner[0])){
+               winnerMap.get(winner[0]).wins++
             } else {
-               if (winnerMap.has(opponent[0])) {
-                  winnerMap.get(opponent[0]).wins++
-               } else {
-                  winnerMap.set(opponent[0], {wins: 1})
-               }
+               winnerMap.set(winner[0], {wins: 1})
             }
-
          })
       }
    })
